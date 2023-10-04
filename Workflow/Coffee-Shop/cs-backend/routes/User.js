78304eb9ab40cff3f22ebb0501b38e84a18c19b0
rachid-dev <70503012+rachid-dev@users.noreply.github.com>
2023-10-04 13:47:43 +0000

    Delete Workflow/Coffee-Shop directory

--- Workflow/Coffee-Shop/cs-backend/routes/User.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const userCtrl = require('../controllers/User')
-const router = express.Router()
-
-router.post('/signup', userCtrl.signUp)
-router.post('/login', userCtrl.logIn)
-
-module.exports = router;
